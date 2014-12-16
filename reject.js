0d807d4e41c3bb8fa2c36bf3e8125df5be63b0e3
Lorenc Estrefi <jlorenc1986@gmail.com>
2014-12-16 21:40:31 +0100

    solve rejected

--- reject.js
+++ reject.js
@@ -1,5 +1,13 @@
 Q = require('q');
 
-var def = Q.defer;
+var def = Q.defer();
+
+// attach the  righ handlers defer(fulfillHandler,rejectHandler)
+def.promise.then(console.log,function(error){
+
+	console.log(error.message);
+})
 
 
+// force the promise to reject
+setTimeout(def.reject,300,{message:"REJECTED!"})
